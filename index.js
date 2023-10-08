df6b42d9f43ca8665b689c8088ec9860491cb962
Osha <wolf.lena.0001@gmail.com>
2023-10-08 15:43:12 +0200

    copy code

--- index.js
+++ index.js
@@ -1,10 +1,59 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const db = require('@cyclic.sh/dynamodb')
 
-app.get('/', function(req, res){
-    res.send('Hallo');
-})
-app.listen(port, ()=> {
-    console.log('Listen on Port ${port}');
-})
+/*This is a built-in middleware function in Express. 
+It parses incoming requests with JSON payloads and is based on body-parser.*/
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.post('/:col/:key', async (req, res) => {
+    console.log(req.body)
+  
+    const col = req.params.col
+    const key = req.params.key
+    console.log(`from collection: ${col} delete key: ${key} with params ${JSON.stringify(req.params)}`)
+    const item = await db.collection(col).set(key, req.body)
+    console.log(JSON.stringify(item, null, 2))
+    res.json(item).end()
+  })
+  
+  // Delete an item
+  app.delete('/:col/:key', async (req, res) => {
+    const col = req.params.col
+    const key = req.params.key
+    console.log(`from collection: ${col} delete key: ${key} with params ${JSON.stringify(req.params)}`)
+    const item = await db.collection(col).delete(key)
+    console.log(JSON.stringify(item, null, 2))
+    res.json(item).end()
+  })
+  
+  // Get a single item
+  app.get('/:col/:key', async (req, res) => {
+    const col = req.params.col
+    const key = req.params.key
+    console.log(`from collection: ${col} get key: ${key} with params ${JSON.stringify(req.params)}`)
+    const item = await db.collection(col).get(key)
+    console.log(JSON.stringify(item, null, 2))
+    res.json(item).end()
+  })
+  
+  // Get a full listing
+  app.get('/:col', async (req, res) => {
+    const col = req.params.col
+    console.log(`list collection: ${col} with params: ${JSON.stringify(req.params)}`)
+    const items = await db.collection(col).list()
+    console.log(JSON.stringify(items, null, 2))
+    res.json(items).end()
+  })
+  
+  // Catch all handler for all other request.
+  app.use('*', (req, res) => {
+    res.json({ msg: 'no route handler found' }).end()
+  })
+  
+  // Start the server
+  const port = process.env.PORT || 3000
+  app.listen(port, () => {
+    console.log(`index.js listening on ${port}`)
+  })

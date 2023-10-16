feb63cd878c043575e86c43c286f8bc86429fcb7
Stefan Subotin <stefanasubotin@gmail.com>
2023-10-16 16:33:30 +0200

    added kreuzwort

---
+++ src/kreuzwort/kreuzwortController.js
@@ -0,0 +1,16 @@
+export default async function(body) {
+    if (body.type == 1) {
+        // const kwNew = require('./kreuzwortNew')
+        // let res = await kwNew(body)
+        // return res;
+        return {
+            Test: 'hat geklappt'
+        }
+    }
+    if (body.type == 0) {
+        const kwUpdate = require('./kreuzwortUpdate')
+        let res = await kwUpdate(body)
+        return res
+    }
+}
+        

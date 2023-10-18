398b6da5b0fc8385ec6cc997f9b9b631b4d0a036
Stefan Subotin <stefanasubotin@gmail.com>
2023-10-18 17:48:56 +0200

    test

--- src/kreuzwort/kreuzwortUpdate.js
+++ src/kreuzwort/kreuzwortUpdate.js
@@ -7,13 +7,13 @@ class KreuzwortUpdate extends Function {
         const messager = require("./kreuzwortMessager");
         const db = require('./kreuzwortDb');
         let quiz = await db.getItem(body.id);
-        const state = this.getState(quiz.props.lines[body.line - 1], body.answer);
+        const state = this.getState(quiz.props.lines[body.line - 1].answer, body.answer);
         await messager.messageCorrection(body.line, state, body.room);
         return 'Done!';
     }
 
     getState(line, answer){
-        console.log(line.answer);
+        console.log(line);
         console.log(answer);
         if (line.answer == answer) return 1;
         return -1;

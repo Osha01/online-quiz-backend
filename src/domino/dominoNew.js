ca5950b8567635031660a0dc04c7d68751e89adc
Osha <wolf.lena.0001@gmail.com>
2023-10-21 18:32:55 +0200

    test 9

--- src/domino/dominoNew.js
+++ src/domino/dominoNew.js
@@ -6,7 +6,7 @@ class domino extends Function {
     const messenger = require("./dominoMessenger");
     const db = require("./dominoDb");
     let questions = await db.getRandomQuestions(body.users.length);
-    let gameId = db.getId();
+    let gameId = db.getNewId();
     await messenger.sendFirstMessage(questions, body.users, body.room, gameId);
     return "Done Domino!";
   }

7c0a9d8fa2ea29484f409afc74e879db092f4623
Stefan Subotin <stefanasubotin@gmail.com>
2023-10-23 17:11:39 +0200

    wwm Backend fertig

--- src/other/simpleQuestionDb.js
+++ src/other/simpleQuestionDb.js
@@ -1,7 +1,10 @@
+const db = require('./database');
+
 class SimpleQuestionDb extends Function {
     async getFullList() {
-        const db = require('./database');
         const items = await db.getFullList('simpleQuestion');
+        console.log('Full List');
+        //console.log(items);
         return items;
     }
 
@@ -11,15 +14,16 @@ class SimpleQuestionDb extends Function {
         for (let i = 0; i < full.results.length; i++){
             let item = await this.getItem(full.results[i].key);
             if (item.props.answerCount == 4) {
-                results.push(item);
+                results.push(full.results[i]);
             }
         }
+        console.log(results);
         return results;
     }
 
     async getItem(key) {
-        const db = require('./database');
         const item = await db.getItem('simpleQuestion', key);
+        return item;
     }
 }
 
--- src/wwm/wwmCreator.js
+++ src/wwm/wwmCreator.js
@@ -3,34 +3,42 @@ class TabooCreator extends Function {
         super(props);
     }
 
-    createQuiz() {
+    async createQuiz() {
         const db = require('../other/simpleQuestionDb');
-        let list = db.getWwmList();
-        let indexes = this.getRandomIndexes(list.length - 1, 10);
+        let list = await db.getWwmList();
+        console.log(list);
+        let indexes = await this.getRandomIndexes(list.length - 1, 10);
 
         let questions = [];
         for (let i = 0; i < indexes.length; i++) {
-            questions.push(db.getItem(list[indexes[i]].key));
+            questions.push(await db.getItem(list[indexes[i]].key));
         }
         let quiz = [];
 
         for (let i = 0; i < indexes.length; i++) {
-            let scramble = this.getRandomIndexes(3, 4);
+            let scramble = await this.getRandomIndexes(3, 4);
+            console.log(scramble);
             let answers = [];
             let correct;
+            console.log(questions[i]);
             for (let j = 0; j < scramble.length; j++) {
                 switch (scramble[j]) {
                     case 0:
+                        console.log(0);
                         answers.push(questions[i].props.answer);
-                        correct = i;
+                        console.log(answers);
+                        correct = j;
                         break;
                     case 1:
+                        console.log(1);
                         answers.push(questions[i].props.falseAnswer1);
                         break;
                     case 2:
+                        console.log(2);
                         answers.push(questions[i].props.falseAnswer2);
                         break;
                     case 3:
+                        console.log(3);
                         answers.push(questions[i].props.falseAnswer3);
                         break;
                 }
@@ -41,17 +49,20 @@ class TabooCreator extends Function {
                 correct: correct
             })
         }
+        console.log(quiz);
         return quiz;
     }
 
-    getRandomIndexes(max, num) {
+    async getRandomIndexes(max, num) {
         const randomizer = require('../other/randomInt');
 
         let indexes = [];
-        for (let i = 0; i <= num; i++) {
+        for (let i = 0; i < num && i <= max; i++) {
             let r = randomizer.getRandomInt(0, max);
             while (this.checkForIndex(r, indexes)) {
-                r = (r + 1) % max;
+                console.log(r + ' bereits vorhanden, i: ' + i + ', max: ' + max + ', num: ' + num);
+                console.log(indexes);
+                r = (r + 1) % (max + 1);
             }
             indexes.push(r);
         }

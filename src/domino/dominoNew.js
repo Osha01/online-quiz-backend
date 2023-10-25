b01a0b15374760ab24673ef1f0652086832ac62b
Osha <wolf.lena.0001@gmail.com>
2023-10-25 22:32:23 +0200

    fix

--- src/domino/dominoNew.js
+++ src/domino/dominoNew.js
@@ -7,24 +7,24 @@ class dominoNew extends Function {
   async getNewDomino(body) {
     const messenger = require("./dominoMessenger");
     let collection = await db.getFullList();
-    let anzahlFragen = body.userCount*4;
-    let questionList =[];
+    let anzahlFragen = body.userCount * 4;
+    let questionList = [];
     let item;
     let laenge = 3;
 
-    if(anzahlFragen<8){
+    if (anzahlFragen < 8) {
       for (let i = 0; i < 8; i++) {
         item = await db.getItem(collection.results[i].key)
         questionList.push(item)
         console.log(item);
 
       }
-    }else
+    } else
       for (let i = 0; i < anzahlFragen; i++) {
         item = await db.getItem(collection.results[i].key)
         questionList.push(item)
         console.log(item);
-    }
+      }
     let switchtedList = this.switchList(questionList)
     console.log(switchtedList);
 
@@ -36,38 +36,38 @@ class dominoNew extends Function {
     );
     return "Done Domino!";
   }
-  switchList(questionList){
-      let keys =[]
-      let fragen=[]
-      let newQuestions=[]
-      let antworten = []
-      for(let i= 0;i<questionList.length;++i){
-        fragen.push(questionList[i].props.question)
-        console.log("Frage hinzugefügt: "+questionList[i].props.question)
-        antworten.push(questionList[i].props.answer)
-        console.log("Antwort hinzugefügt: "+questionList[i].props.answer)
-        keys.push(questionList[i].key)
-        console.log("Key hinzugefügt: "+questionList[i].key)
+  switchList(questionList) {
+    let keys = []
+    let fragen = []
+    let newQuestions = []
+    let antworten = []
+    for (let i = 0; i < questionList.length; ++i) {
+      fragen.push(questionList[i].props.question)
+      console.log("Frage hinzugefügt: " + questionList[i].props.question)
+      antworten.push(questionList[i].props.answer)
+      console.log("Antwort hinzugefügt: " + questionList[i].props.answer)
+      keys.push(questionList[i].key)
+      console.log("Key hinzugefügt: " + questionList[i].key)
+    }
+
+
+    let verschobenAntworten = []
+    let laengeA = antworten.length
+
+    for (let i = 0; i < laengeA; ++i) {
+      if (i == 0) {
+
+      } else {
+        verschobenAntworten[i] = antworten[i - 1];
       }
+    }
+    verschobenAntworten[0] = antworten[(antworten.length - 1)];
 
-
-      let verschobenAntworten = []
-      let laengeA = antworten.length
-
-      for(let i= 0; i<laengeA;++i){
-        if(i==0){
-         
-        }else{
-          verschobenAntworten[i]=antworten[i-1];
-        }
-      }
-      verschobenAntworten[0]=antworten[(antworten.length-1)];
-
-      console.log(fragen+"  unterschiede "+verschobenAntworten)
-      for(let i= 0; i<fragen.length;++i){
-        newQuestions.push({question: fragen[i], answer: antworten[i], key: keys[i]})
-      }
-      return newQuestions
+    console.log(fragen + "  unterschiede " + verschobenAntworten)
+    for (let i = 0; i < fragen.length; ++i) {
+      newQuestions.push({ question: fragen[i], answer: verschobenAntworten[i], key: keys[i] })
+    }
+    return newQuestions
   }
 }
 module.exports = new dominoNew();

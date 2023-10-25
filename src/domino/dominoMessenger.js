5111c4322a35685be6b4d5fbe1fa6516742b7fdb
Osha <wolf.lena.0001@gmail.com>
2023-10-25 16:15:46 +0200

    fix

--- src/domino/dominoMessenger.js
+++ src/domino/dominoMessenger.js
@@ -51,7 +51,7 @@ class dominoMessenger extends Function {
     const Ably = require('ably');
     const ably = new Ably.Realtime.Promise('0sa0Qw.VDigAw:OeO1LYUxxUM7VIF4bSsqpHMSZlqMYBxN-cxS0fKeWDE');
     await ably.connection.once('connected');
-    const channelId = 'domino' + this.state.room;
+    const channelId = 'domino' + room;
     const channel = ably.channels.get(channelId);
 
     console.log("ROOM "+this.state.room)
